4f34822391b07ec0bb0fc4f91bc1c2355115c010
Srinu-7 <srinumaka9908@gmail.com>
2024-08-23 16:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
 function convertToRoman(num) {
-    const obj = {
-        0: ['M', 1000], 
-        1: ['D', 500], 
-        2: ['C', 100], 
-        3: ['L', 50], 
-        4: ['X', 10], 
-        5: ['V', 5], 
-        6: ['I', 1]
-    };
+    const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
     let roman = '';
 
-    for (let i = 0; i <= 6; i++) {
-        while (num >= obj[i][1]) {
-            roman += obj[i][0];
-            num -= obj[i][1];
+    romanNumerals.forEach(([symbol, value]) => {
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
         }
-        
-        // Handle subtractive combinations:
-        if (i === 2 && num >= 900) { // 900 -> CM
-            roman += 'CM';
-            num -= 900;
-        } else if (i === 2 && num >= 400) { // 400 -> CD
-            roman += 'CD';
-            num -= 400;
-        } else if (i === 4 && num >= 90) { // 90 -> XC
-            roman += 'XC';
-            num -= 90;
-        } else if (i === 4 && num >= 40) { // 40 -> XL
-            roman += 'XL';
-            num -= 40;
-        } else if (i === 6 && num >= 9) { // 9 -> IX
-            roman += 'IX';
-            num -= 9;
-        } else if (i === 6 && num >= 4) { // 4 -> IV
-            roman += 'IV';
-            num -= 4;
-        }
-    }
+    });
 
     return roman;
 }
 
-// You can test your code by running the above function and printing it to the console by pressing the run button at the top. To run it with input 36, uncomment the following line
+document.getElementById('converterForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-console.log(convertToRoman(36)); // Expected output: XXXVI
+    const number = parseInt(document.getElementById('number').value, 10);
+    const resultElement = document.getElementById('result');
 
-// do not edit below this line
-module.exports = convertToRoman;
+    if (isNaN(number) || number < 0 || number > 100000) {
+        resultElement.textContent = 'Please enter a valid number between 0 and 100000.';
+    } else {
+        const romanNumeral = convertToRoman(number);
+        resultElement.textContent = `The Roman numeral is: ${romanNumeral}`;
+    }
+});
